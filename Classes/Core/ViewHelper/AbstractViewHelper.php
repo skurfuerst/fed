ec7dd8c039808b3630c9a46c5a21374aa24b7d8c
Claus Due <claus@wildside.dk>
2011-06-06 17:10:44 +0000

    [FEATURE] New class: Tx_Fed_Utility_DocumentHead
    Singleton, which can be injected and allows advanced
    access to manipulating the contents of additionalHeaderData.
    
    Corresponding methods from AbstractViewHelper moved to this
    new class and proxy methods created for backwards compat.
    
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fed/trunk@48409 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Core/ViewHelper/AbstractViewHelper.php
+++ Classes/Core/ViewHelper/AbstractViewHelper.php
@@ -24,7 +24,8 @@
 ***************************************************************/
 
 /**
- *
+ * Provides injected services and methods for easier implementation in
+ * subclassing ViewHelpers
  *
  * @version $Id$
  * @copyright Copyright belongs to the respective authors
@@ -34,13 +35,15 @@
  */
 abstract class Tx_Fed_Core_ViewHelper_AbstractViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractTagBasedViewHelper {
 
-	const TYPE_JAVASCRIPT = 'js';
-	const TYPE_STYLESHEET = 'css';
-
 	/**
 	 * @var Tx_Fed_Utility_JSON
 	 */
 	protected $jsonService;
+
+	/**
+	 * @var Tx_Fed_Utility_DocumentHead
+	 */
+	protected $documentHead;
 
 	/**
 	 * Inject JSON Service
@@ -51,220 +54,134 @@ abstract class Tx_Fed_Core_ViewHelper_AbstractViewHelper extends Tx_Fluid_Core_V
 	}
 
 	/**
+	 * @param Tx_Fed_Utility_DocumentHead $documentHead
+	 */
+	public function injectDocumentHead(Tx_Fed_Utility_DocumentHead $documentHead) {
+		$this->documentHead = $documentHead;
+	}
+
+	/**
 	 * Get a StandaloneView for $templateFile
+	 * DEPRECATED: moved to Tx_Fed_Utility_DocumentHead which is injected at
+	 * $this->documentHead
 	 *
 	 * @param string $templateFile
 	 * @return Tx_Fluid_View_StandaloneView
-	 * @api
+	 * @deprecated
 	 */
 	public function getTemplate($templateFile) {
-		$template = $this->objectManager->get('Tx_Fluid_View_StandaloneView');
-		$template->setTemplatePathAndFilename($templateFile);
-		return $template;
+		return $this->documentHead->getTemplate($templateFile);
 	}
 
 	/**
 	 * Injects $code in header data
+	 * DEPRECATED: moved to Tx_Fed_Utility_DocumentHead which is injected at
+	 * $this->documentHead
 	 *
 	 * @param string $code A rendered tag suitable for <head>
 	 * @param string $type Optional, if left out we assume the code is already wrapped
 	 * @param string $key Optional key for referencing later through $GLOBALS['TSFE']->additionalHeaderData, defaults to md5 cheksum of tag
-	 * @api
+	 * @deprecated
 	 */
 	public function includeHeader($code, $type=NULL, $key=NULL) {
-		if ($type !== NULL) {
-			$code = $this->wrap($code, NULL, $type);
-		}
-		if ($key === NULL) {
-			$key = md5($code);
-		}
-		if (isset($GLOBALS['TSFE']->additionalHeaderData[$key])) {
-			unset($GLOBALS['TSFE']->additionalHeaderData[$key]);
-		}
-		$GLOBALS['TSFE']->additionalHeaderData[$key] = $code;
+		return $this->documentHead->includeHeader($code, $type, $key);
 	}
 
 	/**
 	 * Wrap the code in proper HTML tags. Supports CSS and Javascript only - or returns input $code
+	 * DEPRECATED: moved to Tx_Fed_Utility_DocumentHead which is injected at
+	 * $this->documentHead
 	 *
 	 * @param string $code The code, JS or CSS, to be wrapped
 	 * @param string $filename If specified, file is used instead of source inject
 	 * @param string $type Type of wrapping (css/js)
 	 * @return string
-	 * @api
+	 * @deprecated
 	 */
 	protected function wrap($code=NULL, $file=NULL, $type=NULL) {
-		/**
-		 * Type-support for old js/css inject ViewHelpers - will be removed along with those two
-		 * @deprecated
-		 */
-		if ($type === NULL) {
-			$type = $this->type;
-		}
-		if ($type == self::TYPE_JAVASCRIPT) {
-			if ($file) {
-				return "<script type='text/javascript' src='{$file}'></script>";
-			} else {
-				return "<script type='text/javascript'>\n{$code}\n</script>";
-			}
-		} else if ($type == self::TYPE_STYLESHEET) {
-			if ($file) {
-				return "<link rel='stylesheet' type='text/css' href='{$file}' />";
-			} else {
-				return "<style type='text/css'>\n{$code}\n</style>";
-			}
-		} else {
-			return $code;
-		}
+		return $this->documentHead->wrap($code, $file, $type);
 	}
 
 	/**
 	 * Concatenate files into a string. You can use this in your subclassed extensions to
 	 * combine css/js files in sequence to generate a single output file or code chunk
+	 * DEPRECATED: moved to Tx_Fed_Utility_DocumentHead which is injected at
+	 * $this->documentHead
 	 *
 	 * @param array $files
 	 * @param boolean $saveToFile
 	 * @return string Contents or filename if $saveToFile was specified
-	 * @api
+	 * @deprecated
 	 */
-	protected function concatenate(array $files, $saveToFile=FALSE, $extension=self::TYPE_JAVASCRIPT) {
-		$contents = "";
-		foreach ($files as $file) {
-			$contents .= file_get_contents(PATH_site . $file);
-			$contents .= "\n";
-		}
-		if ($saveToFile) {
-			$md5 = md5(implode('', $files));
-			$contents = $this->saveToTempFile($contents, $md5, $extension);
-		}
-		return $contents;
+	protected function concatenate(array $files, $saveToFile=FALSE, $extension=Tx_Fed_Utility_DocumentHead::TYPE_JAVASCRIPT) {
+		return $this->documentHead->concatenateFiles($files, $saveToFile, $extension);
 	}
 
 	/**
 	 * Save $contents to a temporary file, for example a combined .css file
+	 * DEPRECATED: moved to Tx_Fed_Utility_DocumentHead which is injected at
+	 * $this->documentHead
 	 *
 	 * @param string $contents Contents of the file
 	 * @param string $uniqid Unique id of the temporary file
 	 * @param string $extension Extensin of the filename
-	 * @api
+	 * @deprecated
 	 */
 	protected function saveToTempFile($contents, $uniqid, $extension) {
-		$tempFilePath = "typo3temp/{$uniqid}.{$extension}";
-		$tempFile = PATH_site . $tempFilePath;
-		file_put_contents($tempFile, $contents);
-		return $tempFilePath;
+		return $this->documentHead->saveContentToTempFile($contents, $uniqid, $extension);
 	}
 
 	/**
 	 * Include a list of files with optional concat, compress and cache
+	 * May be deprecated in the future but since the function is so vital, it
+	 * will remain proxied to DocumentHead for now - However, it has been
+	 * demoted from @api to non-api
 	 *
 	 * @param array $filenames Filenames to include
 	 * @param boolean $cache If true, the file is cached (makes sens if $concat or one of the other options is specified)
 	 * @param boolean $concat If true, files are concatenated
 	 * @param boolean $compress If true, files are compressed
 	 * @return string The MD5 checksum of files (which is also the additionalHeaderData array key if you $concat = TRUE)
-	 * @api
 	 */
 	public function includeFiles(array $filenames, $cache=FALSE, $concat=FALSE, $compress=FALSE) {
-		$pathinfo = pathinfo($filename);
-		$type = $pathinfo['extension'];
-		if ($type !== 'css' && $type !== 'js') {
-			$type = 'js'; // assume Javascript for unknown files - this may change later on...
-		}
-		if ($concat === TRUE) {
-			$file = $this->concatenate($filenames, $cache, $type);
-			if ($cache === TRUE) {
-				$this->includeFile($file, $cache, $compress);
-			} else {
-				$code = $this->wrap(NULL, $file); // will be added as header code
-				$this->process($code);
-			}
-		} else {
-			foreach ($filenames as $file) {
-				$this->includeFile($file, $cache, $compress);
-			}
-		}
+		return $this->documentHead->includeFiles($filenames, $cache, $concat, $compress);
 	}
 
 	/**
 	 * Include a single file with optional concat, compress and cache
+	 * Demoted from @api to non-api but remains as proxy into DocumentHead
 	 *
 	 * @param array $filenames Filenames to include
 	 * @param boolean $cache If true, the file is cached (makes sens if $concat or one of the other options is specified)
 	 * @param boolean $concata If true and wildcard filename used, concats all files
 	 * @param boolean $compress If true, files are compressed
 	 * @return void
-	 * @api
 	 */
 	public function includeFile($filename, $cache=FALSE, $concat=FALSE, $compress=FALSE) {
-		$pathinfo = pathinfo($filename);
-		$type = $pathinfo['extension'];
-		if ($pathinfo['filename'] === '*') {
-			$files = $this->getFilenamesOfType($pathinfo['dirname'], $pathinfo['extension']);
-			if ($files) {
-				$this->includeFiles($files, $cache, $concat, $compress);
-			}
-			return;
-		}
-		if ($type !== 'css' && $type !== 'js') {
-			$type = 'js'; // assume Javascript for unknown files - this may change later on...
-		}
-		if ($cache === FALSE && $compress === FALSE) {
-			$code = $this->wrap(NULL, $filename, $type);
-		} else if ($compress === TRUE) {
-			$contents = file_get_contents(PATH_site . $filename);
-			$packed = $this->pack($contents);
-			$md5 = md5($filename);
-			if ($cache === TRUE) {
-				$cachedFile = $this->saveToTempFile($contents, $uniqid, $type);
-				$code = $this->wrap(NULL, $cachedFile, $type);
-			} else {
-				$code = $this->wrap($contents, NULL, $type);
-			}
-		} else {
-			$code = $this->wrap(NULL, $filename, $type);
-		}
-		$this->includeHeader($code);
+		return $this->documentHead->includeFile($filename, $cache, $concat, $compress);
 	}
 
 	/**
 	 * Get an array of all with extension $extension in $dir
+	 *
+	 * Remains as @api method but proxies to DocumentHead. Final location of
+	 * getFilenamesOfType($dir, $extension) may change but this method will remain.
+	 *
 	 * @param string $dir
 	 * @param string $type
 	 * @api
 	 */
 	public function getFilenamesOfType($dir, $extension=NULL) {
-		$relative = $dir;
-		if (substr($dir, 0, 1) != '/') {
-			$dir = PATH_site . $dir;
-		}
-		$files = scandir($dir);
-		foreach ($files as $k=>$file) {
-			$pathinfo = pathinfo($dir.$file);
-			if (is_dir($dir.$file)) {
-				unset($files[$k]);
-			} else if ($extension && $pathinfo['extension'] != $extension) {
-				unset($files[$k]);
-			} else {
-				$files[$k] = "{$relative}/{$file}";
-			}
-		}
-		sort($files);
-		return $files;
+		return $this->documentHead->getFilenamesOfType($dir, $extension);
 	}
 
 	/**
-	 * Pack/compress Javascript code
+	 * Pack/compress Javascript code. Demoted from @api to non-api, fuction
+	 * moved to Tx_Fed_Utility_DocumentHead
 	 * @param string $code
-	 * @api
 	 */
 	public function pack($code) {
-		$encoding = 62; // see value in Tx_Fed_Utility_JavascriptPacker
-		$fastDecode = FALSE;
-		$specialChars = FALSE;
-		$packer = $this->objectManager->get('Tx_Fed_Utility_JavascriptPacker', $encoding, $fastDecode, $specialChars);
-		$packed = $packer->pack();
-		return (string) $packed;
+		return (string) $this->documentHead->pack($code);
 	}
 
 }
---
+++ Classes/Utility/DocumentHead.php
@@ -0,0 +1,274 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Claus Due <claus@wildside.dk>, Wildside A/S
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Allows advanced access to the DOM <head> content while rendering
+ *
+ * @author Claus Due, Wildside A/S
+ * @version $Id$
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ * @package Fed
+ * @subpackage Utility
+ */
+class Tx_Fed_Utility_DocumentHead implements t3lib_Singleton {
+
+	const TYPE_JAVASCRIPT = 'js';
+	const TYPE_STYLESHEET = 'css';
+
+	/**
+	 * @var Tx_Extbase_Object_Manager
+	 */
+	protected $objectManager;
+
+	/**
+	 * @param Tx_Extbase_Object_Manager $objectManager
+	 */
+	public function injectObjectManager(Tx_Extbase_Object_Manager $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * Get a StandaloneView for $templateFile
+	 *
+	 * @param string $templateFile
+	 * @return Tx_Fluid_View_StandaloneView
+	 * @api
+	 */
+	public function getTemplate($templateFile) {
+		$template = $this->objectManager->get('Tx_Fluid_View_StandaloneView');
+		$template->setTemplatePathAndFilename($templateFile);
+		return $template;
+	}
+
+	/**
+	 * Injects $code in header data
+	 *
+	 * @param string $code A rendered tag suitable for <head>
+	 * @param string $type Optional, if left out we assume the code is already wrapped
+	 * @param string $key Optional key for referencing later through $GLOBALS['TSFE']->additionalHeaderData, defaults to md5 cheksum of tag
+	 * @api
+	 */
+	public function includeHeader($code, $type=NULL, $key=NULL) {
+		if ($type !== NULL) {
+			$code = $this->wrap($code, NULL, $type);
+		}
+		if ($key === NULL) {
+			$key = md5($code);
+		}
+		if (isset($GLOBALS['TSFE']->additionalHeaderData[$key])) {
+			unset($GLOBALS['TSFE']->additionalHeaderData[$key]);
+		}
+		$GLOBALS['TSFE']->additionalHeaderData[$key] = $code;
+	}
+
+	/**
+	 * Wrap the code in proper HTML tags. Supports CSS and Javascript only - or returns input $code
+	 *
+	 * @param string $code The code, JS or CSS, to be wrapped
+	 * @param string $filename If specified, file is used instead of source inject
+	 * @param string $type Type of wrapping (css/js)
+	 * @return string
+	 * @api
+	 */
+	public function wrap($code=NULL, $file=NULL, $type=NULL) {
+		/**
+		 * Type-support for old js/css inject ViewHelpers - will be removed along with those two
+		 * @deprecated
+		 */
+		if ($type === NULL) {
+			$type = $this->type;
+		}
+		if ($type == self::TYPE_JAVASCRIPT) {
+			if ($file) {
+				return "<script type='text/javascript' src='{$file}'></script>";
+			} else {
+				return "<script type='text/javascript'>\n{$code}\n</script>";
+			}
+		} else if ($type == self::TYPE_STYLESHEET) {
+			if ($file) {
+				return "<link rel='stylesheet' type='text/css' href='{$file}' />";
+			} else {
+				return "<style type='text/css'>\n{$code}\n</style>";
+			}
+		} else {
+			return $code;
+		}
+	}
+
+	/**
+	 * Concatenate files into a string. You can use this in your subclassed extensions to
+	 * combine css/js files in sequence to generate a single output file or code chunk
+	 *
+	 * @param array $files
+	 * @param boolean $saveToFile
+	 * @return string Contents or filename if $saveToFile was specified
+	 * @api
+	 */
+	public function concatenateFiles(array $files, $saveToFile=FALSE, $extension=self::TYPE_JAVASCRIPT) {
+		$contents = "";
+		foreach ($files as $file) {
+			$contents .= file_get_contents(PATH_site . $file);
+			$contents .= "\n";
+		}
+		if ($saveToFile) {
+			$md5 = md5(implode('', $files));
+			$contents = $this->saveToTempFile($contents, $md5, $extension);
+		}
+		return $contents;
+	}
+
+	/**
+	 * Save $contents to a temporary file, for example a combined .css file
+	 *
+	 * @param string $contents Contents of the file
+	 * @param string $uniqid Unique id of the temporary file
+	 * @param string $extension Extensin of the filename
+	 * @api
+	 */
+	public function saveContentToTempFile($contents, $uniqid, $extension) {
+		$tempFilePath = "typo3temp/{$uniqid}.{$extension}";
+		$tempFile = PATH_site . $tempFilePath;
+		file_put_contents($tempFile, $contents);
+		return $tempFilePath;
+	}
+
+	/**
+	 * Include a list of files with optional concat, compress and cache
+	 *
+	 * @param array $filenames Filenames to include
+	 * @param boolean $cache If true, the file is cached (makes sens if $concat or one of the other options is specified)
+	 * @param boolean $concat If true, files are concatenated
+	 * @param boolean $compress If true, files are compressed
+	 * @return string The MD5 checksum of files (which is also the additionalHeaderData array key if you $concat = TRUE)
+	 * @api
+	 */
+	public function includeFiles(array $filenames, $cache=FALSE, $concat=FALSE, $compress=FALSE) {
+		$pathinfo = pathinfo($filename);
+		$type = $pathinfo['extension'];
+		if ($type !== 'css' && $type !== 'js') {
+			$type = 'js'; // assume Javascript for unknown files - this may change later on...
+		}
+		if ($concat === TRUE) {
+			$file = $this->concatenate($filenames, $cache, $type);
+			if ($cache === TRUE) {
+				$this->includeFile($file, $cache, $compress);
+			} else {
+				$code = $this->wrap(NULL, $file); // will be added as header code
+				$this->process($code);
+			}
+		} else {
+			foreach ($filenames as $file) {
+				$this->includeFile($file, $cache, $compress);
+			}
+		}
+	}
+
+	/**
+	 * Include a single file with optional concat, compress and cache
+	 *
+	 * @param array $filenames Filenames to include
+	 * @param boolean $cache If true, the file is cached (makes sens if $concat or one of the other options is specified)
+	 * @param boolean $concata If true and wildcard filename used, concats all files
+	 * @param boolean $compress If true, files are compressed
+	 * @return void
+	 * @api
+	 */
+	public function includeFile($filename, $cache=FALSE, $concat=FALSE, $compress=FALSE) {
+		$pathinfo = pathinfo($filename);
+		$type = $pathinfo['extension'];
+		if ($pathinfo['filename'] === '*') {
+			$files = $this->getFilenamesOfType($pathinfo['dirname'], $pathinfo['extension']);
+			if ($files) {
+				$this->includeFiles($files, $cache, $concat, $compress);
+			}
+			return;
+		}
+		if ($type !== 'css' && $type !== 'js') {
+			$type = 'js'; // assume Javascript for unknown files - this may change later on...
+		}
+		if ($cache === FALSE && $compress === FALSE) {
+			$code = $this->wrap(NULL, $filename, $type);
+		} else if ($compress === TRUE) {
+			$contents = file_get_contents(PATH_site . $filename);
+			$packed = $this->pack($contents);
+			$md5 = md5($filename);
+			if ($cache === TRUE) {
+				$cachedFile = $this->saveToTempFile($contents, $uniqid, $type);
+				$code = $this->wrap(NULL, $cachedFile, $type);
+			} else {
+				$code = $this->wrap($contents, NULL, $type);
+			}
+		} else {
+			$code = $this->wrap(NULL, $filename, $type);
+		}
+		$this->includeHeader($code);
+	}
+
+	/**
+	 * Get an array of all with extension $extension in $dir
+	 *
+	 * NOTE: this method doesn't really belong here but is required by many
+	 * member methods. Stays for now.
+	 *
+	 * @param string $dir
+	 * @param string $type
+	 */
+	public function getFilenamesOfType($dir, $extension=NULL) {
+		$relative = $dir;
+		if (substr($dir, 0, 1) != '/') {
+			$dir = PATH_site . $dir;
+		}
+		$files = scandir($dir);
+		foreach ($files as $k=>$file) {
+			$pathinfo = pathinfo($dir.$file);
+			if (is_dir($dir.$file)) {
+				unset($files[$k]);
+			} else if ($extension && $pathinfo['extension'] != $extension) {
+				unset($files[$k]);
+			} else {
+				$files[$k] = "{$relative}/{$file}";
+			}
+		}
+		sort($files);
+		return $files;
+	}
+
+	/**
+	 * Pack/compress Javascript code
+	 * @param string $code
+	 * @api
+	 */
+	public function pack($code) {
+		$encoding = 62; // see value in Tx_Fed_Utility_JavascriptPacker
+		$fastDecode = FALSE;
+		$specialChars = FALSE;
+		$packer = $this->objectManager->get('Tx_Fed_Utility_JavascriptPacker', $encoding, $fastDecode, $specialChars);
+		$packed = $packer->pack();
+		return (string) $packed;
+	}
+}
+
+?>
