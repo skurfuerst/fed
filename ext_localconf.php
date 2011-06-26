7b88d5d4a0c13b03eadbffb705a42d03eee5e8bc
Claus Due <claus@wildside.dk>
2011-06-26 13:02:34 +0000

    [CLEANUP] Uniform use of $GLOBALS['TYPO3_CONF_VARS'], ext_localconf.php
    Thanks to Carsten Bleicker for pointing this out.
    
    Fixes: #27707
    
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fed/trunk@49107 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -67,8 +67,8 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 );
 
 if (TYPO3_MODE == 'BE') {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:fed/Classes/Backend/ContentSaveHook.php:Tx_Fed_Backend_ContentSaveHook';
-    $TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = 'EXT:fed/Classes/Backend/Preview.php:Tx_Fed_Backend_Preview';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:fed/Classes/Backend/ContentSaveHook.php:Tx_Fed_Backend_ContentSaveHook';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = 'EXT:fed/Classes/Backend/Preview.php:Tx_Fed_Backend_Preview';
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass'][] = 'EXT:fed/Classes/Backend/DynamicFlexForm.php:Tx_Fed_Backend_DynamicFlexForm';
 }
 
