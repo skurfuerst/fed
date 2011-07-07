669e3623af426768dc0f917c8775b1b93eec6aef
Claus Due <claus@wildside.dk>
2011-07-07 20:34:24 +0000

    [FEATURE] Added ExposedTemplateView which can access info after rendering
    The FlexibleContentElementView now extends this and only adds the 
    getFlexibleContentElementDefinitions method
    
    You can use this ExposedTemplateView for emails, among other things, 
    by registering subjects etc. using vars: fed:data.var name="subject" 
    and then $template->harvest('subject'); from where you started rendering 
    
    Currently only supports "harvest" method which gets a single variable, but 
    will shortly be extended with a lot more methods to harvest a lot more 
    details about the template rendering context and variables.
    
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fed/trunk@49608 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/View/FlexibleContentElementView.php
+++ Classes/View/FlexibleContentElementView.php
@@ -1,30 +1,7 @@
 <?php
 
 
-class Tx_Fed_View_FlexibleContentElementView extends Tx_Fluid_View_StandaloneView {
-
-	protected $flexFormData;
-
-	/**
-	 *
-	 * @param array $flexFormData
-	 */
-	public function setFlexFormData($data) {
-		$this->flexFormData = $data;
-	}
-
-	public function harvest($name) {
-		if ($this->flexFormData) {
-			$this->assignMultiple($this->flexFormData);
-		}
-		$this->render();
-		$templateVariableContainer = $this->baseRenderingContext->getTemplateVariableContainer();
-		if ($templateVariableContainer->exists($name)) {
-			return $templateVariableContainer->get($name);
-		} else {
-			return NULL;
-		}
-	}
+class Tx_Fed_View_FlexibleContentElementView extends Tx_Fed_View_ExposedTemplateView {
 
 	public function getFlexibleContentElementDefinitions() {
 		return $this->harvest('FEDFCE');
