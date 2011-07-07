697b01e526fa50c5eafbfbccf7e559da8201fbfa
Claus Due <claus@wildside.dk>
2011-07-07 20:35:33 +0000

    [FEATURE] Added the ExposedTemplateView class - see previous commit msg.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fed/trunk@49609 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/View/ExposedTemplateView.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class Tx_Fed_View_ExposedTemplateView extends Tx_Fluid_View_StandaloneView {
+
+	protected $flexFormData;
+
+	/**
+	 *
+	 * @param array $flexFormData
+	 */
+	public function setFlexFormData($data) {
+		$this->flexFormData = $data;
+	}
+
+	public function harvest($name) {
+		if ($this->flexFormData) {
+			$this->assignMultiple($this->flexFormData);
+		}
+		$this->render();
+		$templateVariableContainer = $this->baseRenderingContext->getTemplateVariableContainer();
+		if ($templateVariableContainer->exists($name)) {
+			return $templateVariableContainer->get($name);
+		} else {
+			return NULL;
+		}
+	}
+
+	public function getFlexibleContentElementDefinitions() {
+		return $this->harvest('FEDFCE');
+	}
+
+}
+
+
+?>
