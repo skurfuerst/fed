ffd0f70cba61f42637d7589ef56c8169f1aa086e
Claus Due <claus@wildside.dk>
2011-10-15 11:26:04 +0200

    [+FEATURE] Extension Config options to disable various features
    This change brings options to enable/disable Fluid Page templates,
    Fluid Content Elements and the use of automated Backend Controllers.
    
    Change-Id: Ia26698b02d1c25e684803f4af885fab3b5057fa3

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,29 +3,16 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-Tx_Extbase_Utility_Extension::configurePlugin(
-	$_EXTKEY,
-	'Page',
-	array(
-		'Page' => 'list,render',
-	),
-	array(
-	)
-);
 
 Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,
 	'API',
 	array(
 		'Page' => 'render',
-		'FileuploadWidget' => 'upload',
-		'RecordSelectorWidget' => 'search',
 		'Hash' => 'request',
 		'Tool' => 'clearCache,inspectCookie,removeCookie,setCookie,inspectSession,setSession,removeSession'
 	),
 	array(
-		'FileuploadWidget' => 'upload',
-		'RecordSelectorWidget' => 'search',
 		'Hash' => 'request',
 		'Tool' => 'clearCache,inspectCookie,removeCookie,setCookie,inspectSession,setSession,removeSession'
 	)
@@ -65,16 +52,6 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 
 Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,
-	'Sandbox',
-	array(
-		'Sandbox' => 'show',
-	),
-	array(
-	)
-);
-
-Tx_Extbase_Utility_Extension::configurePlugin(
-	$_EXTKEY,
 	'Hash',
 	array(
 		'Hash' => 'request',
@@ -84,57 +61,97 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	)
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'].=',tx_fed_page_controller_action,tx_fed_page_controller_action_sub,tx_fed_page_flexform,';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['fed'] = 'EXT:fed/Classes/Backend/TCEMain.php:Tx_Fed_Backend_TCEMain';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['fed'] = 'EXT:fed/Classes/Backend/TCEMain.php:Tx_Fed_Backend_TCEMain';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass']['fed'] = 'EXT:fed/Classes/Backend/TCEMain.php:Tx_Fed_Backend_TCEMain';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass']['fed'] = 'EXT:fed/Classes/Backend/DynamicFlexForm.php:Tx_Fed_Backend_DynamicFlexForm';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['fed'] = 'EXT:fed/Classes/Backend/Preview.php:Tx_Fed_Backend_Preview';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list.inc']['makeQueryArray']['fed'] = 'EXT:fed/Classes/Backend/MakeQueryArray.php:Tx_Fed_Backend_MakeQueryArray';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['fed_fce'] = 'EXT:fed/Classes/Backend/TemplaVoilaPreview.php:&Tx_Fed_Backend_TemplaVoilaPreview';
-
-t3lib_extMgm::addTypoScript($_EXTKEY,'setup',
-    '[GLOBAL]
-	tt_content.fed_fce = COA
-	tt_content.fed_fce.10 =< lib.stdHeader
-	tt_content.fed_fce.20 < tt_content.list.20.fed_fce
-	tt_content.fed_template = COA
-	tt_content.fed_template.10 =< lib.stdHeader
-	tt_content.fed_template.20 < tt_content.list.20.fed_template
-	tt_content.fed_datasource = COA
-	tt_content.fed_datasource.10 =< lib.stdheader
-	tt_content.fed_datasource.20 < tt_content.list.20.fed_datasource
-	'
-, TRUE);
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup'] = unserialize($_EXTCONF);
 
 
-t3lib_extMgm::addPageTSConfig('
-	TCEFORM.pages.layout.addItems.255 = Fluid Page
-	mod.wizards.newContentElement.wizardItems.special.elements.fed_fce {
-		icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
-		title = Fluid Content Element
-		description = Flexible Content Element using a Fluid template
-		tt_content_defValues {
-			CType = fed_fce
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableFluidPageTemplates']) {
+	t3lib_extMgm::addPageTSConfig('TCEFORM.pages.layout.addItems.255 = Fluid Page');
+	Tx_Extbase_Utility_Extension::configurePlugin(
+		$_EXTKEY,
+		'Page',
+		array(
+			'Page' => 'list,render',
+		),
+		array(
+		)
+	);
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] == '' ? '' : ',') . 'tx_fed_page_controller_action,tx_fed_page_controller_action_sub,tx_fed_page_flexform,';
+}
+
+
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableFluidContentElements']) {
+	t3lib_extMgm::addTypoScript($_EXTKEY,'setup',
+		'[GLOBAL]
+		tt_content.fed_fce = COA
+		tt_content.fed_fce.10 =< lib.stdHeader
+		tt_content.fed_fce.20 < tt_content.list.20.fed_fce
+	', TRUE);
+	t3lib_extMgm::addPageTSConfig('
+		mod.wizards.newContentElement.wizardItems.special.elements.fed_fce {
+			icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
+			title = Fluid Content Element
+			description = Flexible Content Element using a Fluid template
+			tt_content_defValues {
+				CType = fed_fce
+			}
 		}
-	}
-	mod.wizards.newContentElement.wizardItems.special.elements.fed_template {
-		icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
-		title = Fluid Template Display
-		description = Display a standalone Fluid template with optional template variables
-		tt_content_defValues {
-			CType = fed_template
+		mod.wizards.newContentElement.wizardItems.special.show := addToList(fed_fce)
+	');
+}
+
+if (TYPO3_MODE == 'BE') {
+
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['fed'] = 'EXT:fed/Classes/Backend/TCEMain.php:Tx_Fed_Backend_TCEMain';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['fed'] = 'EXT:fed/Classes/Backend/TCEMain.php:Tx_Fed_Backend_TCEMain';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass']['fed'] = 'EXT:fed/Classes/Backend/TCEMain.php:Tx_Fed_Backend_TCEMain';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass']['fed'] = 'EXT:fed/Classes/Backend/DynamicFlexForm.php:Tx_Fed_Backend_DynamicFlexForm';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['fed'] = 'EXT:fed/Classes/Backend/Preview.php:Tx_Fed_Backend_Preview';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list.inc']['makeQueryArray']['fed'] = 'EXT:fed/Classes/Backend/MakeQueryArray.php:Tx_Fed_Backend_MakeQueryArray';
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['fed_fce'] = 'EXT:fed/Classes/Backend/TemplaVoilaPreview.php:&Tx_Fed_Backend_TemplaVoilaPreview';
+
+	t3lib_extMgm::addTypoScript($_EXTKEY,'setup',
+		'[GLOBAL]
+		tt_content.fed_template = COA
+		tt_content.fed_template.10 =< lib.stdHeader
+		tt_content.fed_template.20 < tt_content.list.20.fed_template
+		tt_content.fed_datasource = COA
+		tt_content.fed_datasource.10 =< lib.stdheader
+		tt_content.fed_datasource.20 < tt_content.list.20.fed_datasource
+		'
+	, TRUE);
+
+	t3lib_extMgm::addPageTSConfig('
+		mod.wizards.newContentElement.wizardItems.special.elements.fed_fce {
+			icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
+			title = Fluid Content Element
+			description = Flexible Content Element using a Fluid template
+			tt_content_defValues {
+				CType = fed_fce
+			}
 		}
-	}
-	mod.wizards.newContentElement.wizardItems.special.elements.fed_datasource {
-		icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
-		title = Fluid DataSource Display
-		description = Display a standalone Fluid template with attached DataSource(s)
-		tt_content_defValues {
-			CType = fed_datasource
+		mod.wizards.newContentElement.wizardItems.special.elements.fed_template {
+			icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
+			title = Fluid Template Display
+			description = Display a standalone Fluid template with optional template variables
+			tt_content_defValues {
+				CType = fed_template
+			}
 		}
+		mod.wizards.newContentElement.wizardItems.special.elements.fed_datasource {
+			icon = ../typo3conf/ext/fed/Resources/Public/Icons/Plugin.png
+			title = Fluid DataSource Display
+			description = Display a standalone Fluid template with attached DataSource(s)
+			tt_content_defValues {
+				CType = fed_datasource
+			}
+		}
+		mod.wizards.newContentElement.wizardItems.special.show := addToList(fed_template,fed_datasource)
+	');
+
+	if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableIntegratedBackendLayouts']) {
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS']['ext/cms/classes/class.tx_cms_backendlayout.php'] = t3lib_extMgm::extPath('fed', 'class.ux_cms_backendlayout.php');
 	}
-	mod.wizards.newContentElement.wizardItems.special.show := addToList(fed_fce,fed_template,fed_datasource)
-');
+
+}
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,22 +3,9 @@ if (!defined ('TYPO3_MODE')){
 	die ('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['fed']['plugins']['fed_fce']['pluginType'] = 'CType';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['fed']['plugins']['fed_template']['pluginType'] = 'CType';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['fed']['plugins']['fed_datasource']['pluginType'] = 'CType';
 
-
-Tx_Extbase_Utility_Extension::registerPlugin(
-	$_EXTKEY,
-	'Page',
-	'Fluid Page'
-);
-
-Tx_Extbase_Utility_Extension::registerPlugin(
-	$_EXTKEY,
-	'Fce',
-	'Fluid Content Element'
-);
 
 Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
@@ -34,44 +21,96 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 
 Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
-	'Sandbox',
-	'FED Sandbox'
-);
-
-Tx_Extbase_Utility_Extension::registerPlugin(
-	$_EXTKEY,
 	'Hash',
 	'FED Hasher'
 );
 
-
-t3lib_extMgm::addPlugin(array('Fluid Content Element', 'fed_fce'), 'CType');
 t3lib_extMgm::addPlugin(array('Fluid Template Display', 'fed_template'), 'CType');
 t3lib_extMgm::addPlugin(array('DataSource Display', 'fed_datasource'), 'CType');
 
-t3lib_div::loadTCA('pages');
-t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_addlist']['fed_fce'] = 'pi_flexform';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['fed_sandbox'] = 'pi_flexform';
 
-$before = '--div--;Fluid,--palette--;Fluid layout;fluid,tx_fed_page_flexform,--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.options,';
-$TCA['pages']['types'][1]['showitem'] = $before . $TCA['pages']['types'][1]['showitem'];
-$TCA['pages']['palettes']['fluid']['showitem'] = 'tx_fed_page_controller_action,tx_fed_page_controller_action_sub';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup'] = unserialize($_EXTCONF);
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableFluidContentElements']) {
 
-t3lib_extMgm::addPiFlexFormValue('fed_sandbox', 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/Sandbox.xml');
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['fed']['plugins']['fed_fce']['pluginType'] = 'CType';
+
+	Tx_Extbase_Utility_Extension::registerPlugin(
+		$_EXTKEY,
+		'Fce',
+		'Fluid Content Element'
+	);
+
+	t3lib_extMgm::addPlugin(array('Fluid Content Element', 'fed_fce'), 'CType');
+
+	t3lib_div::loadTCA('tt_content');
+	$TCA['tt_content']['types']['list']['subtypes_addlist']['fed_fce'] = 'pi_flexform';
+	$TCA['tt_content']['types']['fed_fce']['showitem'] = '
+	--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general,
+	--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header,
+	--div--;Fluid Content Element, tx_fed_fcefile;Element type, pi_flexform;Configuration,
+	--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance,
+	--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames,
+	--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
+	--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility,
+	--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access,
+	--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended,tx_fed_fcecontentarea
+	 ';
+}
+
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableFluidPageTemplates']) {
+
+	Tx_Extbase_Utility_Extension::registerPlugin(
+		$_EXTKEY,
+		'Page',
+		'Fluid Page'
+	);
+	t3lib_div::loadTCA('pages');
+	$before = '--div--;Fluid,--palette--;Fluid layout;fluid,tx_fed_page_flexform,--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.options,';
+	$TCA['pages']['types'][1]['showitem'] = $before . $TCA['pages']['types'][1]['showitem'];
+	$TCA['pages']['palettes']['fluid']['showitem'] = 'tx_fed_page_controller_action,tx_fed_page_controller_action_sub';
+
+	t3lib_extMgm::addTCAcolumns('pages', array(
+		'tx_fed_page_format' => Array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_format',
+			'displayCond' => 'FIELD:layout:=:255',
+			'config' => Array (
+				'type' => 'user',
+				'userFunc' => 'Tx_Fed_Backend_PageLayoutSelector->renderFormatField'
+			)
+		),
+		'tx_fed_page_controller_action' => Array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_controller_action',
+			'displayCond' => 'FIELD:layout:=:255',
+			'config' => Array (
+				'type' => 'user',
+				'userFunc' => 'Tx_Fed_Backend_PageLayoutSelector->renderField'
+			)
+		),
+		'tx_fed_page_controller_action_sub' => Array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_controller_action_sub',
+			'displayCond' => 'FIELD:layout:=:255',
+			'config' => Array (
+				'type' => 'user',
+				'userFunc' => 'Tx_Fed_Backend_PageLayoutSelector->renderField'
+			)
+		),
+		'tx_fed_page_flexform' => Array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_flexform',
+			'displayCond' => 'FIELD:layout:=:255',
+			'config' => Array (
+				'type' => 'flex',
+			)
+		),
+	), 1);
+}
+
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'FED Fluid Extbase Development Framework');
 
-$TCA['tt_content']['types']['fed_fce']['showitem'] = '
---palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general,
---palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header,
---div--;Fluid Content Element, tx_fed_fcefile;Element type, pi_flexform;Configuration,
---div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance,
---palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames,
---div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
---palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility,
---palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access,
---div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended,tx_fed_fcecontentarea
- ';
 $TCA['tt_content']['types']['fed_template']['showitem'] = '
 --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general,
 --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header,
@@ -122,43 +161,6 @@ $TCA['tx_fed_domain_model_datasource'] = array(
 );
 
 
-t3lib_extMgm::addTCAcolumns('pages', array(
-	'tx_fed_page_format' => Array (
-		'exclude' => 1,
-		'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_format',
-		'displayCond' => 'FIELD:layout:=:255',
-		'config' => Array (
-			'type' => 'user',
-			'userFunc' => 'Tx_Fed_Backend_PageLayoutSelector->renderFormatField'
-		)
-	),
-	'tx_fed_page_controller_action' => Array (
-		'exclude' => 1,
-		'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_controller_action',
-		'displayCond' => 'FIELD:layout:=:255',
-		'config' => Array (
-			'type' => 'user',
-			'userFunc' => 'Tx_Fed_Backend_PageLayoutSelector->renderField'
-		)
-	),
-	'tx_fed_page_controller_action_sub' => Array (
-		'exclude' => 1,
-		'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_controller_action_sub',
-		'displayCond' => 'FIELD:layout:=:255',
-		'config' => Array (
-			'type' => 'user',
-			'userFunc' => 'Tx_Fed_Backend_PageLayoutSelector->renderField'
-		)
-	),
-	'tx_fed_page_flexform' => Array (
-        'exclude' => 1,
-        'label' => 'LLL:EXT:fed/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_flexform',
-		'displayCond' => 'FIELD:layout:=:255',
-        'config' => Array (
-            'type' => 'flex',
-        )
-    ),
-), 1);
 t3lib_extMgm::addTCAcolumns('tt_content', array(
 	'tx_fed_fcecontentarea' => Array (
 		'exclude' => 1,
